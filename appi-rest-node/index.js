350e36e097dc1d684fbd727daceae64ea438b2ff
edgarin <maed2802@gmail.com>
2023-01-03 17:35:48 -0600

    enero

--- appi-rest-node/index.js
+++ appi-rest-node/index.js
@@ -23,6 +23,24 @@ app.use(express.json())
 
 //crear rutas
 
+app.get("/probando", (req, res) => {
+    console.log("Se ha ejecutado la ruta");
+
+    return res.status(200).json({
+        curso: 'Aprendiendo Node',
+        autor: 'Edgar Juarez',
+        url: 'edgarjuarezweb.com.mx'  
+
+})
+})
+app.get("/", (req, res) => {
+    console.log("Se ha ejecutado la ruta principal");
+
+    return res.status(200).send(
+        '<p>Huevos a todos</p>'
+)
+})
+
 //crear serrvidor y escuchar peticiones http
 app.listen(puerto, ()=>{
     console.log('Servidor corriendo en el puerto '+ puerto);
